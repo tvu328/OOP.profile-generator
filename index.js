1be655854bbbd6d19810c71729d1e47802bb169c
Tyler <tylerlvvu@gmail.com>
2023-01-26 23:53:39 -0800

    first update

---
+++ index.js
@@ -0,0 +1,8 @@
+const Engineer = require("../lib/Engineer");
+const Manager = require("../lib/Manager");
+const Intern = require("../lib/Intern");
+const generateHtml = require("./generateHtml");
+const inquirer = require("inquirer");
+const fs = require("fs");
+
+const team = [];
